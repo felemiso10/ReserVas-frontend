30a1f5e862c2378762abfef4c1da378d29f060d1
Paula Guadalajara <pgs68@alu.ua.es>
2021-03-18 20:40:52 +0100

    [register] Añadido botón para ir a la página de Home

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,11 @@ function CustomHeader({navigation}){
         <Header 
             containerStyle={{backgroundColor:'darkslategrey',width: '100%', borderBottomWidth: 5, marginBottom:'10px' }}
             leftComponent={{ icon: 'menu', color: '#fff', underlayColor: '#3488C0', onPress: () => navigation.dispatch(DrawerActions.openDrawer())}}
-            centerComponent={{ text: 'Reser&vas', 
-                style: { color: '#fff', fontWeight: 'bold', fontSize: 20}}}
+            centerComponent={{ 
+                text: 'Reser&vas', 
+                style: { color: '#fff', fontWeight: 'bold', fontSize: 20},
+                onPress: () => navigation.navigate('Home')
+            }}
         />
     )
 }
