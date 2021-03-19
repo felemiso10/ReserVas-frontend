6c7b8eb87d74a2b9a17063526326ade844e14d7d
Dario Guerrero Montero <dgm94@alu.ua.es>
2021-03-19 14:10:22 +0100

    Algunos cambios de CatCarousel

--- src/components/CatCarousel.js
+++ src/components/CatCarousel.js
@@ -7,8 +7,7 @@ const styles = StyleSheet.create({
     flex: 1,
     minHeight: 275,
     maxHeight: 275,
-    minWidth:350,
-    maxWidth:350,
+ 
     borderWidth: 3,
     borderRadius: 6,
   },
@@ -16,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: "row",
   },
   slide: {
     backgroundColor: 'white',
@@ -29,58 +29,46 @@ export default class Screen extends React.Component {
         <Swiper loop>
           <View style={[styles.slideContainer,styles.slide]}>
           <Image
-                style={{ width: 200, height: 200 }}
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat1.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
+          />
           <Image
-                style={{ width: 200, height: 200 }}
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat2.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
+           />
           <Image
-                style={{ width: 200, height: 200 }}
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat3.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
-          <Image
-                style={{ width: 200, height: 200 }}
+          />
+           <Image
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat4.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
-          <Image
-                style={{ width: 200, height: 200 }}
+          />
+           <Image
+                style={{ width: 200, height: 200,margin:5}}
                 source={require('../../assets/cat5.png')}
-                />
+          />
           </View>
+          
           <View style={[styles.slideContainer,styles.slide]}>
           <Image
-                style={{ width: 200, height: 200 }}
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat6.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
-          <Image
-                style={{ width: 200, height: 200 }}
+          />
+           <Image
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat7.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
-          <Image
-                style={{ width: 200, height: 200 }}
+          />
+           <Image
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat8.png')}
-                />
-          </View>
-          <View style={[styles.slideContainer,styles.slide]}>
-          <Image
-                style={{ width: 200, height: 200 }}
+          />
+           <Image
+                style={{ width: 200, height: 200,margin:5 }}
                 source={require('../../assets/cat9.png')}
-                />
+          />
           </View>
+         
         </Swiper>
       </View>
     );
