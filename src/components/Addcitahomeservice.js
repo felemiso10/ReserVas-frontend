e42ee807465da90ff69f867efd9aa67fb12af796
Dario Guerrero Montero <dgm94@alu.ua.es>
2021-04-18 20:33:38 +0200

    Hecho como modal

--- src/components/Addcitahomeservice.js
+++ src/components/Addcitahomeservice.js
@@ -5,6 +5,9 @@ import styles from '../styles/commonStyles'
 import { AutoComplete } from 'antd';
 import 'antd/dist/antd.css';
 import CustomInput from '../components/forms/CustomInput'
+import Modal from '@material-ui/core/Modal';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 
 const Addcitahomeservice = ({
   user,
@@ -27,27 +30,53 @@ const Addcitahomeservice = ({
 
   }
 }
+
+const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
  
     return (
-        <View style={styles.registerContainer}>
-            <div className="row">
-            <Card>
-            <Card.Title>Añadir cita</Card.Title>
-            <Card.Divider/>
-            <CustomInput 
-              placeholder='Usuario que reservará...' 
-              onChange={changeUserLoginInfo} 
-              idInput='username'
-              object ={user}
-              isRequired = 'true'                  
-            />  
-            <Card.Divider/>
-            <Button onPress={() => 
-               loginComp()
-            } title="Crear cita" />
-            </Card>
-            </div>
-        </View>
+        <div>
+            <button type="button" onClick={handleOpen} >
+                <IconButton color="primary" >
+                    <AddIcon />
+                </IconButton>
+            </button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+            <View style={styles.registerContainer}>
+                <div className="row">
+                <Card>
+                <Card.Title>Añadir cita</Card.Title>
+                <Card.Divider/>
+                <CustomInput 
+                placeholder='Usuario que reservará...' 
+                onChange={changeUserLoginInfo} 
+                idInput='username'
+                object ={user}
+                isRequired = 'true'                  
+                />  
+                <Card.Divider/>
+                <Button onPress={() => 
+                loginComp()
+                } title="Crear cita" />
+                </Card>
+                </div>
+            </View>
+            </Modal>
+
+        </div>
     )
 }
 
